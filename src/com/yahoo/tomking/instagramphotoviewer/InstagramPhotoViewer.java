ee6f205079ba0fbb95451ab2d47d65d4a318d185
Tom King <mobilemind@pobox.com>
2014-11-17 19:26:02 -0800

    start to add Array for popular photos

--- src/com/yahoo/tomking/instagramphotoviewer/InstagramPhotoViewer.java
+++ src/com/yahoo/tomking/instagramphotoviewer/InstagramPhotoViewer.java
@@ -1,5 +1,7 @@
 package com.yahoo.tomking.instagramphotoviewer;
 
+import java.util.ArrayList;
+
 import org.apache.http.Header;
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -17,6 +19,7 @@ import android.view.MenuItem;
 
 public class InstagramPhotoViewer extends Activity {
 	public static final String kIG_CLIENT_ID = "761087dc7aea4f0cb7a09f60f352fb8f";
+	private ArrayList<InstagramPhoto> photos;
 	// https://api.instagram.com/v1/media/popular?client_id=kIG_CLIENT_ID
 	// { "data" => [x] => "user" => "username" }
 	// { "data" => [x] => "caption" => "text" }
@@ -36,6 +39,8 @@ public class InstagramPhotoViewer extends Activity {
 		// https://api.instagram.com/v1/media/popular?client_id=kIG_CLIENT_ID 
     	// setup endpoint
     	String popularPhotosUrl = "https://api.instagram.com/v1/media/popular?client_id=" + kIG_CLIENT_ID;
+    	// init array
+    	photos = new ArrayList<InstagramPhoto>();
     	
     	// create net client & send request
     	AsyncHttpClient client = new AsyncHttpClient();
@@ -53,12 +58,13 @@ public class InstagramPhotoViewer extends Activity {
 						photo.username = photoJSON.getJSONObject("user").getString("username");
 						photo.caption = photoJSON.getJSONObject("caption").getString("text");
 						photo.imageUrl = photoJSON.getJSONObject("images").getJSONObject("standard_resolution").getString("url");
-						photo.imageHeight = photoJSON.getJSONObject("images").getJSONObject("standard_resolution").getInt("url");
-						photo.likesCount = photoJSON.getJSONObject("likes").getInt("count");	
-						Log.i("DEBUG", photo.toString());
+						photo.imageHeight = photoJSON.getJSONObject("images").getJSONObject("standard_resolution").getInt("height");
+						photo.likesCount = photoJSON.getJSONObject("likes").getInt("count");
+						photos.add(photo);
+						Log.i("INFO", "processing photo " + i + "(" + photo.caption + ")");
 					}
 				} catch (JSONException e) {
-					e.printStackTrace();
+					// e.printStackTrace();
 				}
     			
     		};
